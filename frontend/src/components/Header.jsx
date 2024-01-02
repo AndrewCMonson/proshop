bf76ad51f72eab186dffa5b5b41aedb61df5b2ed
Andrew Monson <andrewmonsondev@gmail.com>
2024-01-02 13:13:10 -0500

    added searchbar placeholder and styling

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,20 +1,45 @@
 import React from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import {
+	Navbar,
+	Nav,
+	Container,
+	Form,
+	Row,
+	Col,
+	Button,
+} from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 import { LinkContainer } from 'react-router-bootstrap';
 
 const Header = () => {
 	return (
-		<header>
-			<Navbar  variant="dark" expand="lg" collapseOnSelect className='navBar'>
+		<header className="position-fixed w-100 z-3">
+			<Navbar variant="dark" expand="lg" collapseOnSelect className="navBar">
 				<Container>
 					<LinkContainer to="/">
-						<Navbar.Brand>PROShop</Navbar.Brand>
+						<Navbar.Brand><i>PRO</i>Shop</Navbar.Brand>
 					</LinkContainer>
+					<Form.Group>
+						<Form inline>
+							<Row>
+								<Col xs="auto">
+									<Form.Control
+										type="text"
+										placeholder="Search"
+										className=" mr-sm-2"
+										size='sm'
+									/>
+								</Col>
+								<Col xs="auto">
+									<Button type="submit" size='sm'>Submit</Button>
+								</Col>
+							</Row>
+						</Form>
+					</Form.Group>
 					<Navbar.Toggle aria-controls="basic-navbar-nav" />
 					<Navbar.Collapse id="basic-navbar-nav">
 						<Nav className="ms-auto">
-							<LinkContainer to='/categories'>
+							<LinkContainer to="/categories">
 								<Nav.Link>Categories</Nav.Link>
 							</LinkContainer>
 							<LinkContainer to="/cart">
