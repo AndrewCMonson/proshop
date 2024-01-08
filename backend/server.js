53bb22b22046a1b3f447bca92d75d24e0e0446a6
Andrew Monson <andrewmonsondev@gmail.com>
2024-01-08 16:24:48 -0500

    established productRoutes, created asyncHandler middleware

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,9 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
-import products from './data/products.js';
+
 import { connect } from 'mongoose';
+import productRoutes from './routes/productRoutes.js';
 const PORT = process.env.PORT || 5000;
 
 connectDB();
@@ -14,14 +15,7 @@ app.get('/', (req, res) => {
     res.send(`Server is ready and running on port ${PORT}`);
 });
 
-app.get('/api/products', (req, res) => {
-    res.send(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((product) => product._id === req.params.id);
-    res.send(product);
-});
+app.use('/api/products', productRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
