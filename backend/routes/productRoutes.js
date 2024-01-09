697d4dde68747071a600db41c8131755e481d991
Andrew Monson <andrewmonsondev@gmail.com>
2024-01-09 12:46:13 -0500

    added error middleware

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -17,10 +17,11 @@ router.get(
 		const product = await Product.findById(req.params.id);
 
 		if (product) {
-			res.send(product);
+			return res.json(product);
+		} else {
+			res.status(404);
+			throw new Error('Product not found');
 		}
-
-		res.status(404).json({ message: 'Product not found' });
 	})
 );
 
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
-
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import { connect } from 'mongoose';
 import productRoutes from './routes/productRoutes.js';
 const PORT = process.env.PORT || 5000;
@@ -16,6 +16,9 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
