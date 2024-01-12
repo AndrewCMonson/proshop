3da993812f078a2ae0893433ceaaa4ca72f0c312
Andrew Monson <andrewmonsondev@gmail.com>
2024-01-12 15:05:40 -0500

    added user authentication middleware for users and admins

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -16,7 +16,7 @@ const authUser = asyncHandler(async (req, res) => {
         });
 
         // Set JWT as HTTP only cookie
-        res.cookie('jwt', token, {
+        res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV !== 'development',
             sameSite: 'strict',
