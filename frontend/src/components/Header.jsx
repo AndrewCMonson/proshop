1951c99785ab4018fa2440f06fc3f5638b53b909
Andrew Monson <andrewmonsondev@gmail.com>
2024-01-29 09:51:35 -0500

    login functionality completed

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
-import {
-	Navbar,
-	Nav,
-	Container,
-	Badge
-} from 'react-bootstrap';
+import { Navbar, Nav, Container, Badge, NavDropdown } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 import { LinkContainer } from 'react-router-bootstrap';
 import { useSelector } from 'react-redux';
 
 const Header = () => {
-	const { cartItems } = useSelector((state) => state.cart);
+	const { cartItems } = useSelector(state => state.cart);
+	const { userInfo } = useSelector(state => state.auth);
+
+	const logoutHandler = () => {
+		console.log('logout');
+	};
 
 	return (
 		<header>
 			<Navbar variant="dark" expand="lg" collapseOnSelect className="navBar">
 				<Container>
 					<LinkContainer to="/">
-						<Navbar.Brand><i>PRO</i>Shop</Navbar.Brand>
+						<Navbar.Brand>
+							<i>PRO</i>Shop
+						</Navbar.Brand>
 					</LinkContainer>
 					<Navbar.Toggle aria-controls="basic-navbar-nav" />
 					<Navbar.Collapse id="basic-navbar-nav">
@@ -30,16 +32,27 @@ const Header = () => {
 									<FaShoppingCart /> Cart
 									{cartItems.length > 0 && (
 										<Badge pill bg="danger" className="ms-1">
-											{ cartItems.reduce((acc, item) => acc + item.qty, 0)}
+											{cartItems.reduce((acc, item) => acc + item.qty, 0)}
 										</Badge>
 									)}
 								</Nav.Link>
 							</LinkContainer>
-							<LinkContainer to="/login">
-								<Nav.Link>
-									<FaUser /> Sign In
-								</Nav.Link>
-							</LinkContainer>
+							{userInfo ? (
+								<NavDropdown title={userInfo.name} id="username">
+									<LinkContainer to="/profile">
+										<NavDropdown.Item>Profile</NavDropdown.Item>
+									</LinkContainer>
+									<NavDropdown.Item onClick={logoutHandler}>
+										Logout
+									</NavDropdown.Item>
+								</NavDropdown>
+							) : (
+								<LinkContainer to="/login">
+									<Nav.Link>
+										<FaUser /> Sign In
+									</Nav.Link>
+								</LinkContainer>
+							)}
 						</Nav>
 					</Navbar.Collapse>
 				</Container>
