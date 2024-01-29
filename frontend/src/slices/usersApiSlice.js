076741385018ef1b9ebe687f2c433608dc1a01f9
Andrew Monson <andrewmonsondev@gmail.com>
2024-01-29 10:36:01 -0500

    added register user screen and functionality

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -1,19 +1,26 @@
-import { USERS_LOGIN_URL, USERS_LOGOUT_URL } from "../constants";
+import { USERS_URL } from "../constants";
 import { apiSlice } from "./apiSlice";
 
 export const usersApiSlice = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
         login: builder.mutation({
             query: (data) => ({
-                url: USERS_LOGIN_URL,
+                url: `${USERS_URL}/login`,
                 method: "POST",
                 body: data,
             }),
             keepUnusedDataFor: 5,
         }),
+        register: builder.mutation({
+            query: (data) => ({
+                url: `${USERS_URL}`,
+                method: "POST",
+                body: data,
+            }),
+        }),
         logout: builder.mutation({
             query: () => ({
-                url: USERS_LOGOUT_URL,
+                url: `${USERS_URL}/logout`,
                 method: "POST",
             }),
             
@@ -21,4 +28,4 @@ export const usersApiSlice = apiSlice.injectEndpoints({
     }),
 });
 
-export const { useLoginMutation, useLogoutMutation } = usersApiSlice;
+export const { useLoginMutation, useLogoutMutation, useRegisterMutation } = usersApiSlice;
